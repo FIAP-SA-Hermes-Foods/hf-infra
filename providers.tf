68f6d085f755e5a9d9b85319b4230b95a88c0a3c
leo <leonardo.ierich07@gmail.com>
2024-07-06 10:17:30 -0300

    remove old files

--- providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.region
-}
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
-}
